0603aa50d158c023dbfece47af67265dc1475c44
Gianmarco <gianmarco.contreras@colossusperu.com>
2020-09-20 16:22:41 -0500

    Seguimos con grupos de usuarios y jefes

--- loan_manager/__manifest__.py
+++ loan_manager/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Administrador de préstmos',
+    'name': 'Administrador de préstamos',
     'version': '0.0.1',
     'summary': 'Módulo para gestionar préstamos',
     'description': """Módulo para administrar los préstamos 
@@ -12,12 +12,12 @@
         'data/ir_sequence_data.xml',
         # Archivo de reglas de acceso
         'security/ir.model.access.csv',
-        'security/loan_manager_registros_groups.xml',
-        'security/loan_manager_registro_security.xml',
+        # -'security/loan_manager_registros_groups.xml',
+        # -'security/loan_manager_registro_security.xml',
         # Vistas
         'views/loan_manager_menus.xml',
         'views/cliente_views.xml',
-        'views/sede_views.xml',
+        # -'views/sede_views.xml',
         'views/prestamo_views.xml',
     ],
     'installable': True,
